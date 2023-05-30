6dc276f9209e28701951e74280bf286f80ec2727
Alistairj43 <39683918+Alistairj43@users.noreply.github.com>
2023-05-30 13:24:56 -0700

    Fixed implicit curavture method
    Now works for structures with transformations to rotation, scale and offset.

--- dependancies/curvatureImplicit.m
+++ dependancies/curvatureImplicit.m
@@ -11,36 +11,22 @@ function [GC, MC, k1, k2] = curvatureImplicit(TPMS,points)
 % Designed for use in the TPMS Designer Toolbox
 %
 %   Credit: Alistair Jones, 30/09/2021, RMIT University
-%% Find Various Properties - Gaussian, Mean, Inclination
+%% Find Various Properties - gaussian curvature, mean curvature, k1, k2
 
-syms x y z;
-assume(x,'real'); assume(y,'real'); assume(z,'real');
+syms x0 y0 z0 x y z;
+assume(x0,'real'); assume(y0,'real'); assume(z0,'real');
 
 %Apply the scaling of the unit cell before differentiation
-%xyz = 2*pi*[x y z]./[TPMS.cellSize];
+A = TPMS.tform.A(1:3,1:3);
+xyz = [x y z]/A;
 
 % Create implicit functions for MC and GC using differential geometry
-G = gradient(TPMS.u(x,y,z));
+G = gradient(TPMS.u(xyz(1),xyz(2),xyz(3)),[x,y,z]);
 G2 = [gradient(G(1),[x y z]) gradient(G(2),[x y z]) gradient(G(3),[x y z])];
 K = det([G2' G; G' 0]);
 H = -1/2*divergence(G/norm(G));
 MCf = matlabFunction(H,'Vars',[x y z]);
 GCf = matlabFunction(-K/(G'*G).^2,'Vars',[x y z]);
-
-
-%Rotations and phase shifts are shape preserving - these can be applied by
-%inversely transforming the points of interest.
-%rZ = [cosd(TPMS.Rxyz(3)) -sind(TPMS.Rxyz(3)) 0; sind(TPMS.Rxyz(3)) cosd(TPMS.Rxyz(3)) 0; 0 0 1];
-%rY = [cosd(TPMS.Rxyz(2)) 0 -sind(TPMS.Rxyz(2)); 0 1 0; sind(TPMS.Rxyz(2)) 0 cosd(TPMS.Rxyz(2))];
-%rX = [1 0 0; 0 cosd(TPMS.Rxyz(1)) -sind(TPMS.Rxyz(1)); 0 sind(TPMS.Rxyz(1)) cosd(TPMS.Rxyz(1))];
-trans = [0 0 0]; %trans = [TPMS.offset(1) TPMS.offset(2) TPMS.offset(3)];
-
-
-
-
-pad = [0; 0; 0]; T = eye(3);%T = rX*rY*rZ;
-tform=affine3d([T pad; trans 1]);
-points = transformPointsInverse(tform,points);
 
 %Calculate the properties at the transformed points of interest
 GC = GCf(points(:,1),points(:,2),points(:,3));
